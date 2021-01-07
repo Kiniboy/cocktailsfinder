1b7f9fe9a5263eb3de2070d5f7d754e65bb4cf12
Suchet <lucas.suquet@gmail.com>
2021-01-07 03:21:36 +0100

    update cards and button screen position

--- resources/views/cocktails.blade.php
+++ resources/views/cocktails.blade.php
@@ -1,111 +1,71 @@
 @include('layout.header')
 <body class="bodyWithBackground">
-<div class="container">
-    <div class="row">
-        <h1>Cocktails</h1>
-    </div>
-    <div class="d-flex justify-content-center">
-        <div class="col-1">
-            <button class="btn btn-primary bouton-image monBouton" type="submit">Filtrer</button>
-        </div>
-        <div class="col-1">
-            <button class="btn btn-primary" type="submit">Nom</button>
-        </div>
-        <div class="col-1">
-            <button class="btn btn-primary" type="submit">Alcool</button>
-        </div>
-        <div class="col-1 ">
-            <button class="btn btn-primary" type="submit">Couleur</button>
+    <div class="btn-toolbar">
+        <div class="btn-group">
+            <button href="#" class="button">1</button>
+            <button href="#" class="button">2</button>
+            <button href="#" class="button">3</button>
+            <button href="#" class="button">4</button>
+            <button href="#" class="button">5</button>
         </div>
     </div>
-</div>
-<div class="container-fluid">
-    <div class="row colone_test">
-        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-5">
-            <a href="/cocktail">
-                <div class="card" style="width: 18rem;">
-                    <img class="img_cards_cocktails" alt="profile Pic">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the
-                            card's content.</p>
-                        <img class="img_cards_cocktails_like" alt="profile Pic">
-                    </div>
+    <div class="container-fluid">
+        <div class="card-group">
+            <div class="card">
+                <img class="card-img-top" src="" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                 </div>
-            </a>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                        content. This card has even longer content thango the first to show that equal height action.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins a</small></p>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                        content. This card has even longer content than the first to show that equal height action.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
         </div>
-        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-5">
-            <a href="/cocktail">
-                <div class="card" style="width: 18rem;">
-                    <img class="img_cards_cocktails" alt="profile Pic">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the
-                            card's content.</p>
-                        <img class="img_cards_cocktails_like" alt="profile Pic">
-                    </div>
+        <div class="card-group">
+            <div class="card">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                        content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                 </div>
-            </a>
-        </div>
-        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-5">
-            <a href="/cocktail">
-                <div class="card" style="width: 18rem;">
-                    <img class="img_cards_cocktails" alt="profile Pic">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the
-                            card's content.</p>
-                        <img class="img_cards_cocktails_like" alt="profile Pic">
-                    </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                 </div>
-            </a>
-        </div>
-        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-5">
-            <a href="/cocktail">
-                <div class="card" style="width: 18rem;">
-                    <img class="img_cards_cocktails" alt="profile Pic">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the
-                            card's content.</p>
-                        <img class="img_cards_cocktails_like" alt="profile Pic">
-                    </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                        content. This card has even longer content than the first to show that equal height action.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                 </div>
-            </a>
-        </div>
-        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-5">
-            <a href="/cocktail">
-                <div class="card" style="width: 18rem;">
-                    <img class="img_cards_cocktails" alt="profile Pic">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the
-                            card's content.</p>
-                        <img class="img_cards_cocktails_like" alt="profile Pic">
-                    </div>
-                </div>
-            </a>
-        </div>
-        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-5">
-            <a href="/cocktail">
-                <div class="card" style="width: 18rem;">
-                    <img class="img_cards_cocktails" alt="profile Pic">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the
-                            card's content.</p>
-                        <img class="img_cards_cocktails_like" alt="profile Pic">
-                    </div>
-                </div>
-            </a>
+            </div>
         </div>
     </div>
-</div>
 </body>
 @include('layout.footer')
