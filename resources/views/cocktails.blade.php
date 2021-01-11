91c927867acf7d8419f567c23faf0018153d972e
Suchet <lucas.suquet@gmail.com>
2021-01-11 14:53:19 +0100

    update cards for boostrap < 4

--- resources/views/cocktails.blade.php
+++ resources/views/cocktails.blade.php
@@ -11,7 +11,7 @@
     </div>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-sm-6 col-md-4 col-lg-3 py-2">
+            <div class="col-sm-6 col-lg-3 py-2">
                 <div class="card h-100">
                     <img class="card-img-top img-fluid" src="//placehold.it/500x200" alt="Card image cap">
                     <div class="card-body">
@@ -21,7 +21,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6 col-md-4 col-lg-3 py-2">
+            <div class="col-sm-6 col-lg-3 py-2">
                 <div class="card h-100">
                     <div class="card-body">
                         <h4 class="card-title">Card title</h4>
@@ -30,7 +30,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6 col-md-4 col-lg-3 py-2">
+            <div class="col-sm-6 col-lg-3 py-2">
                 <div class="card h-100">
                     <img class="card-img-top img-fluid" src="//placehold.it/500x180" alt="Card image cap">
                     <div class="card-body">
@@ -41,7 +41,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6 col-md-4 col-lg-3 py-2">
+            <div class="col-sm-6 col-lg-3 py-2">
                 <div class="card h-100">
                     <div class="card-body">
                         <h4 class="card-title">Card title</h4>
@@ -52,7 +52,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-sm-6 col-md-4 col-lg-3 py-2">
+            <div class="col-sm-6 col-lg-3 py-2">
                 <div class="card h-100">
                     <img class="card-img-top img-fluid" src="//placehold.it/500x200" alt="Card image cap">
                     <div class="card-body">
@@ -62,7 +62,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6 col-md-4 col-lg-3 py-2">
+            <div class="col-sm-6 col-lg-3 py-2">
                 <div class="card h-100">
                     <div class="card-body">
                         <h4 class="card-title">Card title</h4>
@@ -71,7 +71,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6 col-md-4 col-lg-3 py-2">
+            <div class="col-sm-6 col-lg-3 py-2">
                 <div class="card h-100">
                     <img class="card-img-top img-fluid" src="//placehold.it/500x180" alt="Card image cap">
                     <div class="card-body">
@@ -82,7 +82,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6 col-md-4 col-lg-3 py-2">
+            <div class="col-sm-6 col-lg-3 py-2">
                 <div class="card h-100">
                     <div class="card-body">
                         <h4 class="card-title">Card title</h4>
