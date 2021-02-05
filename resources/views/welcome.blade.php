10e0eb1c0176eee7ececb1b47c37a314b8d43dd7
SYLVESTRE Samuel <s.sylvestre@hotmail.com>
2021-02-05 19:39:42 +0100

    add content css front page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,10 @@
 @include('layout.header')
     <body>
+
     <div class="container pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-        <div class="jumbotron bg-dark">
-            <h1 class="display-4">Bienvenue sur CocktailFinder</h1>
+        <h1 class="app-title">CocktailsFinder</h1>
+        <div class="jumbotron" style="background-color: rgba(255, 255, 128, .5);">
+
             <p class="lead">le site regroupant les différents cocktails disponibles dans la ville de Toulouse.</p>
             <hr class="my-4">
             <p> Cherchez ou laissez vous tenter par un de nos nombreux cocktails et <br>nous vous indiquerons ou vous pourrez le trouver !</p>
