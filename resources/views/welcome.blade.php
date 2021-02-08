21c7be928dd57653c36b0a121cc77147c558cce9
SYLVESTRE Samuel <s.sylvestre@hotmail.com>
2021-02-08 17:47:36 +0100

    content homepage new buttons css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,29 +4,32 @@
     <div class="container text-center">
         <div class="app-top">
             <h1 class="app-title">CocktailsFinder</h1>
+            <div class="article-buttons"><a class="white" href="#">
+                    <p><span class="bg"></span><span class="base"></span><span class="text">Chercher un Cocktail</span></p></a><a class="white" href="#">
+                    <p><span class="bg"></span><span class="base"></span><span class="text">Chercher un Bar</span></p></a>
+            </div>
         </div>
         <div class="app-center">
             <div class="container" id="article-right">
-                <img class="container__image" src="../../public/img/acceuil.jpg">
+                <img class="container__image" src="../img/accueil.jpg">
                 <div class="container__text">
                     <p class="lead">VOS SAVOUREUX COCKTAILS SUR TOULOUSE</p>
                     <hr class="my-4">
                     <p> Cherchez ou laissez vous tenter par un de nos nombreux cocktails et <br>nous vous indiquerons ou vous pourrez le trouver !</p>
                     <p class="lead">
-                        <a class="btn btn-primary btn-lg" href="cocktails" role="button">CHERCHER</a>
+                    <div class="article-buttons"><a href="#">
+                            <p><span class="bg"></span><span class="base"></span><span class="text">Trouver un Bar</span></p></a><a class="white" href="#">
+                            <p><span class="bg"></span><span class="base"></span><span class="text">Trouver un Cocktail</span></p></a>
+                    </div>
                     </p>
                 </div>
             </div>
 
-            <div class="container" id="article-left">
-                <img class="container__image" src="https://mtdata.ru/u7/photo53A0/20946911428-0/original.jpg#20946911428">
-                <div class="container__text">
-                    <p class="lead">DE NOMBREUX BARS A COCKTAIL REPERTORIÉS</p>
-                    <hr class="my-4">
-                    <p>Pourquoi se rendre à une seule adresse ? Faite le tour de votre ville !</p>
-                    <p class="lead">
-                        <a class="btn btn-primary btn-lg" href="cocktails" role="button">CHERCHER</a>
-                    </p>
+            <div class="container">
+                <div class="article-buttons"><a href="#">
+                        <p><span class="bg"></span><span class="base"></span><span class="text">Trouver un Bar</span></p></a><a class="white" href="#">
+                        <p><span class="bg"></span><span class="base"></span><span class="text">Trouver un Cocktail</span></p></a><a class="transparent" href="#">
+                        <p><span class="bg"></span><span class="base"></span><span class="text">Noter</span></p></a>
                 </div>
             </div>
 {{--            <div class="jumbotron" style="background-color: rgba(255, 255, 255, .5);">--}}
